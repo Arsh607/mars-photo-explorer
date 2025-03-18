746bd7baf9c976c0dfcbedae0caa43469871a0a7
arishi <arishi@academic.rrc.ca>
2025-03-18 17:53:14 -0500

    Added API details in the code.

---
+++ script.js
@@ -0,0 +1,7 @@
+const API_KEY = "fQaKtrXUE5IscIzFhdiweDsAO7ZOWZ1E9C4tvGqV"; 
+const BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
+
+// DOM Elements
+const dateForm = document.getElementById("date-form");
+const dateInput = document.getElementById("date-input");
+const photoGallery = document.getElementById("photo-gallery");
