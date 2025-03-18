1c9b5fa409fba4ad63a308bc4574877c2c4636ed
arishi <arishi@academic.rrc.ca>
2025-03-18 18:15:24 -0500

    Final commit: Did the final cleanup.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const API_KEY = "fQaKtrXUE5IscIzFhdiweDsAO7ZOWZ1E9C4tvGqV"; 
 const BASE_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
 
-
 const dateForm = document.getElementById("date-form");
 const dateInput = document.getElementById("date-input");
 const photoGallery = document.getElementById("photo-gallery");
